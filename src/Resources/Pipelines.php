ad1d7bb9d21486ad8f6b1bd97f2ae6201b65a82d
Fernando Araujo <fmaraujo@gmail.com>
2017-02-22 13:36:02 -0300

    inclusão do método movement_statistics na classe Pipelines

--- src/Resources/Pipelines.php
+++ src/Resources/Pipelines.php
@@ -15,4 +15,11 @@ class Pipelines extends Resource
      * @var array
      */
     protected $disabled = ['deleteBulk'];
+
+    public function movement_statistics($id, $start_date, $end_date, $user_id)
+    {
+        return $this->request->get(':id/movement_statistics', compact(
+            'id', 'start_date', 'end_date', 'user_id'
+        ));
+    }
 }
